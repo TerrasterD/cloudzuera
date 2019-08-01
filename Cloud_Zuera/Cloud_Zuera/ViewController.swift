3a58cd7bf492d9e7ce9bdd03a0a4bf03ed045d5a
Victor Dângelo <terrasterd@gmail.com>
2019-08-01 14:49:49 -0300

    Mudanças com as bibliotecas

--- Cloud_Zuera/Cloud_Zuera/ViewController.swift
+++ Cloud_Zuera/Cloud_Zuera/ViewController.swift
@@ -7,11 +7,29 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var tblJSON: UITableView!
+    var arrRes = [[String:AnyObject]]() //Array of dictionary 
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        Alamofire.request("http://api.androidhive.info/contacts/").responseJSON { (responseData) -> Void in
+            if((responseData.result.value) != nil) {
+                let swiftyJsonVar = JSON(responseData.result.value!)
+                
+                if let resData = swiftyJsonVar["contacts"].arrayObject {
+                    self.arrRes = resData as! [[String:AnyObject]]
+                }
+                if self.arrRes.count > 0 {
+                    self.tblJSON.reloadData()
+                }
+            }
+        }
         // Do any additional setup after loading the view.
     }
 
