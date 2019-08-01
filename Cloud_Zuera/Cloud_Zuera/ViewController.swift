d10de46a42d73c56638c5900bbd8fc9735649acb
Victor Dângelo <terrasterd@gmail.com>
2019-08-01 14:24:05 -0300

    arquivos para o projeto

---
+++ Cloud_Zuera/Cloud_Zuera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Cloud_Zuera
+//
+//  Created by Victor Dângelo Teixeira Ciccarini on 01/08/19.
+//  Copyright © 2019 Victor Dângelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
